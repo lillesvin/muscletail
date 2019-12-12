08452dd79420d137926d8be7cd4c101c2a03cf44
Anders K. Madsen <akm@novicell.dk>
2019-12-12 16:45:55 +0100

    Support various config placements + defaults

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
+import (
+	"flag"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+var configFile string
+var defaultConfig []string
+
+func init() {
+	defaultConfig = []string{
+		"./.muscletail.toml",
+		"./muscletail.toml",
+		"/etc/muscletail.toml",
+	}
+
+	flag.StringVar(&configFile, "config", "", "Use this config")
+	flag.StringVar(&configFile, "c", "", "Shorthand for -config")
+	flag.Parse()
+}
+
 func main() {
-	cfg := ReadConfig("config/config.default.toml")
+	if configFile == "" {
+		for _, f := range defaultConfig {
+			info, err := os.Stat(f)
+			if !os.IsNotExist(err) && !info.IsDir() {
+				configFile = f
+				break
+			}
+		}
+	}
+
+	if configFile == "" {
+		log.Fatal("No config found")
+	}
+	log.WithFields(log.Fields{
+		"Config": configFile,
+	}).Info("Found config file")
+
+	cfg := ReadConfig(configFile)
 
 	for _, w := range cfg.Watches {
 		go w.Monitor()
